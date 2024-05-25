11fa42d22d85703e26c370120679161c15163256
Thomas Eizinger <thomas@eizinger.io>
2024-05-25 19:50:59 +1000

    Basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,74 @@
-use std::io;
+use std::{io, ops::Neg};
 
 /// Implements our "real" bank.
-///
-/// A bank is essentially a ledger. Ledgers are typically implemented using a transaction system, i.e. balances are only aggregated from all transactions on-demand.
 #[derive(Debug, Clone, Default)]
-pub struct Bank {}
+pub struct Bank {
+    accounts: Vec<u64>,
+    transactions: Vec<Transaction>,
+}
 
 impl Bank {
+    pub fn open(&mut self, _can_overdraw: bool) -> io::Result<u64> {
+        let new_account = self.accounts.last().map_or(0, |id| *id + 1);
+        self.accounts.push(new_account);
+
+        Ok(new_account)
+    }
+
     pub fn deposit(&mut self, id: u64, amount: u64) -> io::Result<()> {
-        todo!()
+        self.transactions
+            .push(Transaction::Deposit { to: id, amount });
+
+        Ok(())
     }
 
     pub fn withdraw(&mut self, id: u64, amount: u64) -> io::Result<()> {
-        todo!()
+        self.transactions
+            .push(Transaction::Withdraw { from: id, amount });
+
+        Ok(())
     }
 
     pub fn transfer(&mut self, from: u64, to: u64, amount: u64) -> io::Result<()> {
-        todo!()
+        self.transactions
+            .push(Transaction::Transfer { from, to, amount });
+
+        Ok(())
     }
 
-    pub fn close(&mut self, id: u64) -> io::Result<()> {
-        todo!()
-    }
+    pub fn balance(&self, id: u64) -> io::Result<i64> {
+        let balance = self
+            .transactions
+            .iter()
+            .filter_map(|t| match t {
+                Transaction::Deposit { to, amount } => (*to == id).then_some(*amount as i64),
+                Transaction::Withdraw { from, amount } => {
+                    (*from == id).then_some((*amount as i64).neg())
+                }
+                Transaction::Transfer { from, to, amount } => {
+                    if *from == id {
+                        return Some((*amount as i64).neg());
+                    }
 
-    pub fn open(&mut self, can_overdraw: bool) -> io::Result<u64> {
-        todo!()
-    }
+                    if *to == id {
+                        return Some(*amount as i64);
+                    }
 
-    pub fn unfreeze(&mut self, id: u64) -> io::Result<()> {
-        todo!()
-    }
+                    None
+                }
+            })
+            .sum();
 
-    pub fn freeze(&mut self, id: u64) -> io::Result<()> {
-        todo!()
+        Ok(balance)
     }
+}
 
-    pub fn balance(&self, id: u64) -> io::Result<u64> {
-        todo!()
-    }
+/// A transaction within our bank.
+///
+/// It is typical for ledgers to implemented as a series of transactions.
+#[derive(Debug, Clone)]
+enum Transaction {
+    Deposit { to: u64, amount: u64 },
+    Withdraw { from: u64, amount: u64 },
+    Transfer { from: u64, to: u64, amount: u64 },
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,9 +2,28 @@ use proptest::{
     arbitrary::any,
     sample,
     strategy::{Just, Strategy},
+    test_runner::Config,
 };
-use proptest_state_machine::{ReferenceStateMachine, StateMachineTest};
+use proptest_state_machine::{prop_state_machine, ReferenceStateMachine, StateMachineTest};
 use proptest_state_machine_banking::Bank;
+
+prop_state_machine! {
+    #![proptest_config(Config {
+        // Enable verbose mode to make the state machine test print the
+        // transitions for each case.
+        verbose: 1,
+        .. Config::default()
+    })]
+
+    #[test]
+    fn run_bank_test(
+        // This is a macro's keyword - only `sequential` is currently supported.
+        sequential
+        // The number of transitions to be generated for each case. This can
+        // be a single numerical value or a range as in here.
+        1..20 => RealBank
+    );
+}
 
 /// Holds the state of our "simulated" bank.
 ///
@@ -12,15 +31,18 @@ use proptest_state_machine_banking::Bank;
 #[derive(Debug, Clone)]
 struct SimBank {
     accounts: Vec<SimAccount>,
+
+    next_account_id: u64,
 }
 
 /// A simulated account.
 ///
 /// This holds - contrary to our production code - the actual balance.
 /// This is a much simpler model to assert against.
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 struct SimAccount {
-    balance: u64,
+    id: u64,
+    balance: i64,
 }
 
 /// Holds the state of a "real" bank.
@@ -49,15 +71,15 @@ enum Transition {
         to: sample::Index,
         amount: u64,
     },
-    Freeze {
-        account_id: sample::Index,
-    },
-    Unfreeze {
-        account_id: sample::Index,
-    },
-    Close {
-        account_id: sample::Index,
-    },
+    // Freeze {
+    //     account_id: sample::Index,
+    // },
+    // Unfreeze {
+    //     account_id: sample::Index,
+    // },
+    // Close {
+    //     account_id: sample::Index,
+    // },
 }
 
 impl ReferenceStateMachine for SimBank {
@@ -67,6 +89,7 @@ impl ReferenceStateMachine for SimBank {
     fn init_state() -> proptest::prelude::BoxedStrategy<Self::State> {
         Just(Self {
             accounts: Default::default(),
+            next_account_id: 0,
         })
         .boxed()
     }
@@ -75,27 +98,54 @@ impl ReferenceStateMachine for SimBank {
         proptest::prop_oneof![
             Just(Transition::Open),
             (any::<sample::Index>(), any::<u64>())
-                .prop_map(|(account_id, amount)| Transition::Deposit { account_id, amount }),
+                .prop_map(|(account_id, amount)| { Transition::Deposit { account_id, amount } }),
+            (any::<sample::Index>(), any::<u64>())
+                .prop_map(|(account_id, amount)| { Transition::Withdraw { account_id, amount } }),
+            (any::<sample::Index>(), any::<sample::Index>(), any::<u64>())
+                .prop_map(|(from, to, amount)| Transition::Transfer { from, to, amount }),
         ]
         .boxed()
     }
 
     fn apply(mut state: Self::State, transition: &Self::Transition) -> Self::State {
         match transition {
-            Transition::Open => state.accounts.push(SimAccount::default()),
-            Transition::Withdraw { account_id, amount } => todo!(),
-            Transition::Deposit { account_id, amount } => {
-                let account = account_id.get_mut(&mut state.accounts);
-
-                account.balance += *amount;
+            Transition::Open => {
+                state.accounts.push(SimAccount {
+                    balance: 0,
+                    id: state.next_account_id,
+                });
+                state.next_account_id += 1;
             }
-            Transition::Transfer { from, to, amount } => todo!(),
-            Transition::Freeze { account_id } => todo!(),
-            Transition::Unfreeze { account_id } => todo!(),
-            Transition::Close { account_id } => todo!(),
+            Transition::Withdraw { account_id, amount } => {
+                account_id.get_mut(&mut state.accounts).balance -= (*amount) as i64;
+            }
+            Transition::Deposit { account_id, amount } => {
+                account_id.get_mut(&mut state.accounts).balance += (*amount) as i64;
+            }
+            Transition::Transfer { from, to, amount } => {
+                from.get_mut(&mut state.accounts).balance -= (*amount) as i64;
+                to.get_mut(&mut state.accounts).balance += (*amount) as i64;
+            }
         }
 
         state
+    }
+
+    fn preconditions(state: &Self::State, transition: &Self::Transition) -> bool {
+        match transition {
+            Transition::Open => true,
+            Transition::Withdraw { .. } => !state.accounts.is_empty(),
+            Transition::Deposit { .. } => !state.accounts.is_empty(),
+            Transition::Transfer { from, to, .. } => {
+                let have_accounts = !state.accounts.is_empty();
+
+                if !have_accounts {
+                    return false;
+                }
+
+                from.get(&state.accounts).id != to.get(&state.accounts).id
+            }
+        }
     }
 }
 
@@ -138,21 +188,6 @@ impl StateMachineTest for RealBank {
 
                 state.inner.transfer(*from, *to, amount).unwrap();
             }
-            Transition::Freeze { account_id } => {
-                let account = account_id.get(&state.open_accounts);
-
-                state.inner.freeze(*account).unwrap();
-            }
-            Transition::Unfreeze { account_id } => {
-                let account = account_id.get(&state.open_accounts);
-
-                state.inner.unfreeze(*account).unwrap();
-            }
-            Transition::Close { account_id } => {
-                let account = account_id.get(&state.open_accounts);
-
-                state.inner.close(*account).unwrap();
-            }
         }
 
         // Assert our state.
@@ -165,7 +200,10 @@ impl StateMachineTest for RealBank {
             let actual_balance = state.inner.balance(*id).unwrap();
             let expected_balance = sim_account.balance;
 
-            assert_eq!(actual_balance, expected_balance);
+            assert_eq!(
+                actual_balance, expected_balance,
+                "balance mismatch on account {id}"
+            );
         }
 
         state
